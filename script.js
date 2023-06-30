645966902d4e803f2927fcaceca1f704101215f5
shev-andry <136947471+shev-andry@users.noreply.github.com>
2023-06-30 18:07:38 +0700

    make a function that randomly pick a choice

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 /* Make a rock paper scissors game
    the opponent is computer
-   make a function that randomly select rock paper or scissor
+   make an array of the thing(RPS)
+   get random num between 0 to 2
+   index the array using the random num
+   assign that value to computer choice
    make a prompt that let user input value, make it case insensitive
    compare both value (computer and user)
    say "you win" if user won or "you lose" if user lose
-   */
+*/
+
+let gameChoice = ['Rock', 'Paper', 'Scissors'];
+
+function getComputerChoice() {
+  let computerChoice = Math.floor(Math.random() * ((3 - 1) + 1));
+  computerChoice = gameChoice.at(computerChoice);
+  console.log(computerChoice)
+}
+
+console.log(getComputerChoice())
