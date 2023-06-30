414b3cb132b5ae2a5aba33e98be70209e13e2a83
shev-andry <136947471+shev-andry@users.noreply.github.com>
2023-06-30 17:27:22 +0700

    make pseudocode

---
+++ script.js
@@ -0,0 +1,7 @@
+/* Make a rock paper scissors game
+   the opponent is computer
+   make a function that randomly select rock paper or scissor
+   make a prompt that let user input value, make it case insensitive
+   compare both value (computer and user)
+   say "you win" if user won or "you lose" if user lose
+   */
