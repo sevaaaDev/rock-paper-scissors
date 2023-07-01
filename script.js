3462c072102fca62af4c8ca27cbb7eb6918cd393
shev-andry <136947471+shev-andry@users.noreply.github.com>
2023-07-01 11:36:16 +0700

    add round function to play 1 round

--- script.js
+++ script.js
@@ -4,19 +4,57 @@
    get random num between 0 to 2
    index the array using the random num
    assign that value to computer choice
-   make a prompt that let user input value, make it case insensitive
-   compare both value (computer and user)
+*/
+/*
+   make a prompt that let user input value
+   check for typo or case sensitive
+   if there is a typo, alert user
+   if no typo, assign the value to userChoice
+*/
+/*
+   make all possibility of choice
+   compare it
    say "you win" if user won or "you lose" if user lose
 */
 
-let gameChoice = ['Rock', 'Paper', 'Scissors'];
+let gameChoice = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
-  let computerChoice = Math.floor(Math.random() * ((3 - 1) + 1));
-  return computerChoice = gameChoice.at(computerChoice);
+  let computerChoice = Math.floor(Math.random() * (3 - 1 + 1));
+  return (computerChoice = gameChoice.at(computerChoice));
 }
 
-console.log('Computer : ' + getComputerChoice())
+let computerChoice = getComputerChoice();
+let userChoice = prompt("Rock, Paper, Scissors");
 
+checkUserTypo(userChoice);
 
+function checkUserTypo() {
+  let fixedChoice =
+    userChoice.charAt(0).toUpperCase() + userChoice.slice(1).toLowerCase();
+  if (
+    !(
+      fixedChoice === "Paper" ||
+      fixedChoice === "Rock" ||
+      fixedChoice === "Scissors"
+    )
+  ) {
+    alert("You have a typo or did not write anything");
+  } else {
+    return (userChoice = fixedChoice);
+  }
+}
 
+function playRound(computerSelection, userSelection) {
+  if (
+    (userSelection === "Rock" && computerSelection === "Paper") ||
+    (userSelection === "Scissors" && computerSelection === "Rock") ||
+    (userSelection === "Paper" && computerSelection === "Scissors")
+  ) {
+    return alert(
+      `Computer : ${computerChoice} \nYou : ${userChoice} \nYou lose`
+    );
+  }
+}
+
+playRound(computerChoice, userChoice);
