7aaeb2ec9ba2cb11c7c2367a8a416f465b8eccc0
shev-andry <136947471+shev-andry@users.noreply.github.com>
2023-07-06 14:36:22 +0700

    remove function that play 5 round of game

--- script.js
+++ script.js
@@ -1,68 +1,64 @@
 let userScore = 0;
 let computerScore = 0;
 
-game();
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * (3 - 1 + 1));
+  return gameChoice.at(choice);
+}
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    function getComputerChoice() {
-      let choice = Math.floor(Math.random() * (3 - 1 + 1));
-      return gameChoice.at(choice);
-    }
-    function getUserChoice() {
-      let input = prompt("Rock, Paper, Scissors");
-      let fixedChoice =
-        input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
-      if (
-        !(
-          fixedChoice === "Paper" ||
-          fixedChoice === "Rock" ||
-          fixedChoice === "Scissors"
-          )
-          ) {
-            alert("You have a typo or did not write anything");
-            getUserChoice();
-          } else {
-            return fixedChoice;
-          }
-        }   
-    function playRound(computerSelection, userSelection) {
-      if (
-        (userSelection === "Rock" && computerSelection === "Paper") ||
-        (userSelection === "Scissors" && computerSelection === "Rock") ||
-        (userSelection === "Paper" && computerSelection === "Scissors")
-        ) {
-        computerScore++;
-        return `You lose, ${computerChoice} beats ${userChoice}`;
-      } else if (
-        (userSelection === "Scissors" && computerSelection === "Paper") ||
-        (userSelection === "Rock" && computerSelection === "Scissors") ||
-        (userSelection === "Paper" && computerSelection === "Rock")
-      ) {
-        userScore++;
-        return `You win, ${userChoice} beats ${computerChoice}`;
-      } else {
-        return `it's a draw`;
-      }
-    }
-
-    let gameChoice = ["Rock", "Paper", "Scissors"];
-    let computerChoice = getComputerChoice();
-    let userChoice = getUserChoice();
-    alert(playRound(computerChoice, userChoice));
-
-  }
-  if (userScore > computerScore) {
-    alert(
-      `Computer : ${computerScore} point \nYou : ${userScore} point \nYou win`
-    );
-  } else if (userScore === computerScore) {
-    alert(
-      `Computer : ${computerScore} point \nYou : ${userScore} point \nIt's a draw`
-    );
+function getUserChoice() {
+  let input = prompt("Rock, Paper, Scissors");
+  let fixedChoice =
+    input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
+  if (
+    !(
+      fixedChoice === "Paper" ||
+      fixedChoice === "Rock" ||
+      fixedChoice === "Scissors"
+    )
+  ) {
+    alert("You have a typo or did not write anything");
+    getUserChoice();
   } else {
-    alert(
-      `Computer : ${computerScore} point \nYou : ${userScore} point \nYou lose`
-    );
+    return fixedChoice;
   }
+}
+
+function playRound(computerSelection, userSelection) {
+  if (
+    (userSelection === "Rock" && computerSelection === "Paper") ||
+    (userSelection === "Scissors" && computerSelection === "Rock") ||
+    (userSelection === "Paper" && computerSelection === "Scissors")
+  ) {
+    computerScore++;
+    return `You lose, ${computerChoice} beats ${userChoice}`;
+  } else if (
+    (userSelection === "Scissors" && computerSelection === "Paper") ||
+    (userSelection === "Rock" && computerSelection === "Scissors") ||
+    (userSelection === "Paper" && computerSelection === "Rock")
+  ) {
+    userScore++;
+    return `You win, ${userChoice} beats ${computerChoice}`;
+  } else {
+    return `it's a draw`;
+  }
+}
+
+let gameChoice = ["Rock", "Paper", "Scissors"];
+let computerChoice = getComputerChoice();
+let userChoice = getUserChoice();
+alert(playRound(computerChoice, userChoice));
+
+if (userScore > computerScore) {
+  alert(
+    `Computer : ${computerScore} point \nYou : ${userScore} point \nYou win`
+  );
+} else if (userScore === computerScore) {
+  alert(
+    `Computer : ${computerScore} point \nYou : ${userScore} point \nIt's a draw`
+  );
+} else {
+  alert(
+    `Computer : ${computerScore} point \nYou : ${userScore} point \nYou lose`
+  );
 }
