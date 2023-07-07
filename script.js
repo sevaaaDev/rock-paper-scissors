34a8b3760fe3ea4a679f9514999bec5498f84758
shev-andry <136947471+shev-andry@users.noreply.github.com>
2023-07-07 16:17:01 +0700

    add message after game over

--- script.js
+++ script.js
@@ -3,17 +3,16 @@ let computerScore = 0;
 let result
 const userBoard = document.querySelector(".userScore");
 const compBoard = document.querySelector(".compScore");
-const info = document.querySelector(".Info");
+const btn = document.querySelectorAll('button')
 
-game();
+playGame()
 
-function game() {
+function playGame() {
   function getUserChoice() {
     document.addEventListener("click", assignUserChoice);
     function assignUserChoice(e) {
       if (e.target.tagName !== "BUTTON") return;
       userChoice = e.target.className;
-      console.log(userChoice);
       playRound(computerChoice, userChoice);
     }
   }
@@ -40,6 +39,7 @@ function game() {
       computerScore++;
       result = 'lose'
       showInfo()
+      checkResult()
       computerChoice = getComputerChoice();
     } else if (
       (userSelection === "Scissors" && computerSelection === "Paper") ||
@@ -49,10 +49,12 @@ function game() {
       userScore++;
       result = 'win'
       showInfo()
+      checkResult()
       computerChoice = getComputerChoice();
     } else {
       result = 'draw'
       showInfo()
+      checkResult()
       computerChoice = getComputerChoice();
     }
   }
@@ -60,8 +62,27 @@ function game() {
     let choice = Math.floor(Math.random() * 2.4);
     return gameChoice[choice];
   }
+  
+  function checkResult(){
+    if (userScore == 5 || computerScore == 5) {
+      userBoard.style.display = 'none'
+      compBoard.style.display = 'none'
+      for (let i = 0; i < btn.length; i++) {
+        btn[i].style.display = 'none'
+      }
+      const message = document.createElement('div')
+      container.append(message)
+      if (userScore < computerScore) {
+        message.innerText = 'You lose'
+      } else {
+        message.innerText = 'You win'
+      }
+
+    } 
+  }
 
   let gameChoice = ["Rock", "Paper", "Scissors"];
   let computerChoice = getComputerChoice();
   let userChoice = getUserChoice();
 }
+
