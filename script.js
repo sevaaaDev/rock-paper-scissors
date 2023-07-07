6834f1779edf3a4c4234a0746756d28292aa3469
shev-andry <136947471+shev-andry@users.noreply.github.com>
2023-07-07 16:28:56 +0700

    formatting

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let userScore = 0;
 let computerScore = 0;
-let result
+let result;
 const userBoard = document.querySelector(".userScore");
 const compBoard = document.querySelector(".compScore");
-const btn = document.querySelectorAll('button')
+const btn = document.querySelectorAll("button");
 
-playGame()
+playGame();
 
 function playGame() {
   function getUserChoice() {
@@ -18,13 +18,13 @@ function playGame() {
   }
 
   function showInfo() {
-    if (result == 'lose' ) {
+    if (result == "lose") {
       userBoard.textContent = `You : ${userScore}`;
-      compBoard.textContent = `Computer : ${computerScore}`;;
-    } else if (result == 'win') {
+      compBoard.textContent = `Computer : ${computerScore}`;
+    } else if (result == "win") {
       userBoard.textContent = `You : ${userScore}`;
-      compBoard.textContent = `Computer : ${computerScore}`;;
-    } else if (result == 'draw') {
+      compBoard.textContent = `Computer : ${computerScore}`;
+    } else if (result == "draw") {
       userBoard.textContent = `You : ${userScore}`;
       compBoard.textContent = `Computer : ${computerScore}`;
     }
@@ -37,9 +37,9 @@ function playGame() {
       (userSelection === "Paper" && computerSelection === "Scissors")
     ) {
       computerScore++;
-      result = 'lose'
-      showInfo()
-      checkResult()
+      result = "lose";
+      showInfo();
+      checkResult();
       computerChoice = getComputerChoice();
     } else if (
       (userSelection === "Scissors" && computerSelection === "Paper") ||
@@ -47,14 +47,14 @@ function playGame() {
       (userSelection === "Paper" && computerSelection === "Rock")
     ) {
       userScore++;
-      result = 'win'
-      showInfo()
-      checkResult()
+      result = "win";
+      showInfo();
+      checkResult();
       computerChoice = getComputerChoice();
     } else {
-      result = 'draw'
-      showInfo()
-      checkResult()
+      result = "draw";
+      showInfo();
+      checkResult();
       computerChoice = getComputerChoice();
     }
   }
@@ -62,27 +62,24 @@ function playGame() {
     let choice = Math.floor(Math.random() * 2.4);
     return gameChoice[choice];
   }
-  
-  function checkResult(){
+
+  function checkResult() {
     if (userScore == 5 || computerScore == 5) {
-      userBoard.style.display = 'none'
-      compBoard.style.display = 'none'
+      userBoard.style.display = "none";
+      compBoard.style.display = "none";
       for (let i = 0; i < btn.length; i++) {
-        btn[i].style.display = 'none'
+        btn[i].style.display = "none";
       }
-      const message = document.createElement('div')
-      container.append(message)
+      const message = document.createElement("div");
+      container.append(message);
       if (userScore < computerScore) {
-        message.innerText = 'You lose'
+        message.innerText = "You lose";
       } else {
-        message.innerText = 'You win'
+        message.innerText = "You win";
       }
-
-    } 
+    }
   }
-
   let gameChoice = ["Rock", "Paper", "Scissors"];
   let computerChoice = getComputerChoice();
   let userChoice = getUserChoice();
 }
-
